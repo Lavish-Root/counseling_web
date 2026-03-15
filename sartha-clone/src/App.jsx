a90c9d45442ab65d19924fe549a85e49836da833
coderlavish <153733864+coderlavish@users.noreply.github.com>
2026-03-15 15:36:54 +0530

    Implement ScrollToTop component to reset scroll on route change

--- sartha-clone/src/App.jsx
+++ sartha-clone/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -16,6 +17,7 @@ function App() {
 
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Navbar />
         <main className="flex-grow">
